3f03dd7dbfcd26eda5a432eaebae2d7b48eec983
benign-fork-5056 <gareeeliu@gmail.com>
2020-02-03 22:49:16 +0000

    Automatically backed up by Learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,5 +1,7 @@
 # code your #position_taken? method here!
+# class notes
+# def position_taken?(board, index)
+#   ((board[index] = "X") || ("O"))
+# end
 
-def position_taken?(board, index)
-  ((board[index] = "X") || ("O"))
-end
+def position_taken? (board, index)
